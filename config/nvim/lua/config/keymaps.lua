3f41537a6a389a338b1e7e7c066e9dcf2e6a8537
Maya <huizar@seas.upenn.edu>
2026-02-25 15:14:36 -0500

    feat: add Catppuccin colorscheme with persistent light/dark toggle

--- config/nvim/lua/config/keymaps.lua
+++ config/nvim/lua/config/keymaps.lua
@@ -1,3 +1,17 @@
--- Keymaps are set automatically by LazyVim.
--- Add your custom keymaps here.
--- (populated in later tasks)
+-- Toggle Catppuccin flavor between mocha (dark) and latte (light)
+vim.keymap.set("n", "<leader>tt", function()
+  local current = require("catppuccin").options.flavour
+  local next_flavor = current == "mocha" and "latte" or "mocha"
+
+  -- Persist choice
+  local state_file = vim.fn.stdpath("data") .. "/catppuccin-flavor"
+  local f = io.open(state_file, "w")
+  if f then
+    f:write(next_flavor)
+    f:close()
+  end
+
+  require("catppuccin").setup({ flavour = next_flavor })
+  vim.cmd.colorscheme("catppuccin")
+  vim.notify("Catppuccin: " .. next_flavor, vim.log.levels.INFO)
+end, { desc = "Toggle light/dark theme" })
---
+++ config/nvim/lua/plugins/colorscheme.lua
@@ -0,0 +1,58 @@
+return {
+  -- Disable LazyVim's default colorscheme
+  { "folke/tokyonight.nvim", enabled = false },
+
+  -- Catppuccin
+  {
+    "catppuccin/nvim",
+    name = "catppuccin",
+    priority = 1000,
+    opts = {
+      flavour = "mocha", -- default; overridden at startup by persisted flavor
+      integrations = {
+        cmp = true,
+        gitsigns = true,
+        nvimtree = false,
+        neo_tree = true,
+        telescope = { enabled = true },
+        treesitter = true,
+        which_key = true,
+        mason = true,
+        mini = { enabled = true },
+        lsp_saga = false,
+        native_lsp = {
+          enabled = true,
+          underlines = {
+            errors = { "undercurl" },
+            hints = { "undercurl" },
+            warnings = { "undercurl" },
+            information = { "undercurl" },
+          },
+        },
+      },
+    },
+    config = function(_, opts)
+      -- Load persisted flavor
+      local state_file = vim.fn.stdpath("data") .. "/catppuccin-flavor"
+      local f = io.open(state_file, "r")
+      if f then
+        local flavor = f:read("*l")
+        f:close()
+        if flavor and flavor ~= "" then
+          opts.flavour = flavor
+        end
+      end
+
+      require("catppuccin").setup(opts)
+      vim.cmd.colorscheme("catppuccin")
+    end,
+  },
+
+  -- Tell LazyVim to use catppuccin
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "catppuccin",
+    },
+  },
+}
